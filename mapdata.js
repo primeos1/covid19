96e2a7de7e3f2e5c5151c0b19875b2829e697704
Miley 6th <akhenisio@yahoo.com>
2020-05-06 01:27:25 +0100

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,502 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "500", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "yes",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    NGA2839: {
+      color: "#ff0000",
+      name: "Borno",
+      zoomable: "no",
+      url: "index8.html"
+    },
+    NGA2841: {
+      color: "#ff6666",
+      name: "Abia",
+      url: "index1.html"
+    },
+    NGA2842: {
+      name: "Akwa Ibom",
+      description: " ",
+      url: "index3.html"
+    },
+    NGA2843: {
+      name: "Imo",
+      description: " ",
+      url: "index17.html"
+    },
+    NGA2844: {
+      name: "Rivers",
+      description: " ",
+      url: "index33.html"
+    },
+    NGA2845: {
+      name: "Bayelsa",
+      description: " ",
+      url: "index6.htnl"
+    },
+    NGA2846: {
+      name: "Benue",
+      description: " ",
+      url: "index7.html"
+    },
+    NGA2847: {
+      name: "Cross River",
+      description: " ",
+      url: "index9.html"
+    },
+    NGA2848: {
+      name: "Taraba",
+      description: " ",
+      url: "index35.html"
+    },
+    NGA2849: {
+      name: "Kwara",
+      description: " ",
+      url: "index24.html"
+    },
+    NGA2850: {
+      color: "#990000",
+      name: "Lagos",
+      description: " ",
+      url: "index25.html"
+    },
+    NGA2851: {
+      name: "Niger",
+      description: " ",
+      url: "index27.html"
+    },
+    NGA2852: {
+      name: "Ogun",
+      description: " ",
+      url: "index28.html"
+    },
+    NGA2853: {
+      name: "Ondo",
+      description: " ",
+      url: "index29.html"
+    },
+    NGA2854: {
+      name: "Ekiti",
+      description: " ",
+      url: "index13.html"
+    },
+    NGA2855: {
+      name: "Osun",
+      description: " ",
+      url: "index30.html"
+    },
+    NGA2856: {
+      name: "Oyo",
+      description: " ",
+      url: "index31.html"
+    },
+    NGA2857: {
+      name: "Anambra",
+      description: " ",
+      url: "index4.html"
+    },
+    NGA2858: {
+      name: "Bauchi",
+      description: " ",
+      url: "index5.html"
+    },
+    NGA2859: {
+      name: "Gombe",
+      description: " ",
+      url: "index16.html"
+    },
+    NGA2860: {
+      name: "Delta",
+      description: " ",
+      url: "index10.html"
+    },
+    NGA2861: {
+      name: "Edo",
+      description: " ",
+      url: "index12.html"
+    },
+    NGA2862: {
+      name: "Enugu",
+      description: " ",
+      url: "index14.html"
+    },
+    NGA2863: {
+      name: "Ebonyi",
+      description: " ",
+      url: "index11.html"
+    },
+    NGA2864: {
+      name: "Kaduna",
+      description: " ",
+      url: "index19.html"
+    },
+    NGA2865: {
+      name: "Kogi",
+      description: " ",
+      url: "index23.html"
+    },
+    NGA2866: {
+      name: "Plateau",
+      description: " ",
+      url: "index32.html"
+    },
+    NGA2867: {
+      name: "Nassarawa",
+      description: " ",
+      url: "index26.html"
+    },
+    NGA2868: {
+      name: "Jigawa",
+      description: " ",
+      url: "index18.html"
+    },
+    NGA2869: {
+      name: "Kano",
+      description: " ",
+      url: "index20.html"
+    },
+    NGA2870: {
+      name: "Katsina",
+      description: " ",
+      url: "index21.html"
+    },
+    NGA2871: {
+      name: "Sokoto",
+      description: " ",
+      url: "index34.html"
+    },
+    NGA2872: {
+      name: "Zamfara",
+      description: " ",
+      url: "index36.html"
+    },
+    NGA2873: {
+      name: "Yobe",
+      description: " ",
+      url: "index35.html"
+    },
+    NGA2879: {
+      name: "Kebbi",
+      description: " ",
+      url: "index22.html"
+    },
+    NGA2881: {
+      name: "Adamawa",
+      description: " ",
+      url: "index2.html"
+    },
+    NGA3470: {
+      name: "Federal Capital Territory",
+      description: " ",
+      url: "fct.html"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "9.083333",
+      lng: "7.533333",
+      name: "Abuja"
+    }
+  },
+  labels: {
+    "0": {
+      name: "Borno",
+      parent_id: "NGA2839",
+      x: 884.2,
+      y: 164.5
+    },
+    "1": {
+      name: "Abia",
+      parent_id: "NGA2841",
+      x: 408.1,
+      y: 696.1
+    },
+    "2": {
+      name: "Akwa Ibom",
+      parent_id: "NGA2842",
+      x: 429.1,
+      y: 763.3
+    },
+    "3": {
+      name: "Imo",
+      parent_id: "NGA2843",
+      x: 364.5,
+      y: 709.2
+    },
+    "4": {
+      name: "Rivers",
+      parent_id: "NGA2844",
+      x: 334.5,
+      y: 746.7
+    },
+    "5": {
+      name: "Bayelsa",
+      parent_id: "NGA2845",
+      x: 279.5,
+      y: 771
+    },
+    "6": {
+      name: "Benue",
+      parent_id: "NGA2846",
+      x: 545.5,
+      y: 561.2
+    },
+    "7": {
+      name: "Cross River",
+      parent_id: "NGA2847",
+      x: 476.4,
+      y: 705.5
+    },
+    "8": {
+      name: "Taraba",
+      parent_id: "NGA2848",
+      x: 660.3,
+      y: 504.9
+    },
+    "9": {
+      name: "Kwara",
+      parent_id: "NGA2849",
+      x: 181.5,
+      y: 448.7
+    },
+    "10": {
+      name: "Lagos",
+      parent_id: "NGA2850",
+      x: 107.2,
+      y: 622.2
+    },
+    "11": {
+      name: "Niger",
+      parent_id: "NGA2851",
+      x: 241.1,
+      y: 346.2
+    },
+    "12": {
+      name: "Ogun",
+      parent_id: "NGA2852",
+      x: 38.1,
+      y: 583.5
+    },
+    "13": {
+      name: "Ondo",
+      parent_id: "NGA2853",
+      x: 179.6,
+      y: 600.1
+    },
+    "14": {
+      name: "Ekiti",
+      parent_id: "NGA2854",
+      x: 211.2,
+      y: 524.6
+    },
+    "15": {
+      name: "Osun",
+      parent_id: "NGA2855",
+      x: 155.8,
+      y: 542.5
+    },
+    "16": {
+      name: "Oyo",
+      parent_id: "NGA2856",
+      x: 72.9,
+      y: 484.9
+    },
+    "17": {
+      name: "Anambra",
+      parent_id: "NGA2857",
+      x: 358.3,
+      y: 658.3
+    },
+    "18": {
+      name: "Bauchi",
+      parent_id: "NGA2858",
+      x: 580.6,
+      y: 296.2
+    },
+    "19": {
+      name: "Gombe",
+      parent_id: "NGA2859",
+      x: 704.3,
+      y: 264.7
+    },
+    "20": {
+      name: "Delta",
+      parent_id: "NGA2860",
+      x: 277.3,
+      y: 713.1
+    },
+    "21": {
+      name: "Edo",
+      parent_id: "NGA2861",
+      x: 295.3,
+      y: 599.6
+    },
+    "22": {
+      name: "Enugu",
+      parent_id: "NGA2862",
+      x: 394.2,
+      y: 625.6
+    },
+    "23": {
+      name: "Ebonyi",
+      parent_id: "NGA2863",
+      x: 448.1,
+      y: 644.6
+    },
+    "24": {
+      name: "Kaduna",
+      parent_id: "NGA2864",
+      x: 423.8,
+      y: 284.1
+    },
+    "25": {
+      name: "Kogi",
+      parent_id: "NGA2865",
+      x: 383.8,
+      y: 538.4
+    },
+    "26": {
+      name: "Plateau",
+      parent_id: "NGA2866",
+      x: 583.6,
+      y: 417.3
+    },
+    "27": {
+      name: "Nassarawa",
+      parent_id: "NGA2867",
+      x: 443.2,
+      y: 458.3
+    },
+    "28": {
+      name: "Jigawa",
+      parent_id: "NGA2868",
+      x: 575.8,
+      y: 134.3
+    },
+    "29": {
+      name: "Kano",
+      parent_id: "NGA2869",
+      x: 479.5,
+      y: 169.3
+    },
+    "30": {
+      name: "Katsina",
+      parent_id: "NGA2870",
+      x: 408.7,
+      y: 104.8
+    },
+    "31": {
+      name: "Sokoto",
+      parent_id: "NGA2871",
+      x: 228.1,
+      y: 60
+    },
+    "32": {
+      name: "Zamfara",
+      parent_id: "NGA2872",
+      x: 305,
+      y: 149.7
+    },
+    "33": {
+      name: "Yobe",
+      parent_id: "NGA2873",
+      x: 737.2,
+      y: 116.5
+    },
+    "34": {
+      name: "Kebbi",
+      parent_id: "NGA2879",
+      x: 122,
+      y: 151.7
+    },
+    "35": {
+      name: "Adamawa",
+      parent_id: "NGA2881",
+      x: 803.2,
+      y: 379.9
+    },
+    "36": {
+      name: "Federal Capital Territory",
+      parent_id: "NGA3470",
+      x: 368.8,
+      y: 435.1
+    }
+  },
+  regions: {},
+  data: {
+    data: {
+      NGA2839: "100",
+      NGA2841: "23",
+      NGA2850: "300"
+    }
+  }
+};
